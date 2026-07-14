484705ca00aaa46e8590d720c0d1409b52aa201e
Reyes <benjamin.reyes@isas.de>
2026-07-14 12:59:11 +0200

    shell script corrections

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker pull benjaminreyess/sir_jupyter:latest 
-docker run -rm -p 8888:8888 -v "$(pwd):/workspace"  --name sir_jupyter benjaminreyess/sir_jupyter:latest 
+docker run --rm -p 8888:8888 -v "$(pwd):/workspace"  --name sir_jupyter benjaminreyess/sir_jupyter:latest 
